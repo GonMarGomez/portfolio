b5753b306272ed9d5de51a6489a888f051d0478a
Gonzalo Gomez <gonzalomgomez5@gmail.com>
2024-01-19 21:15:00 -0300

    Agregando ultimo certificado y proyectos

--- main.js
+++ main.js
@@ -2,21 +2,27 @@ const imageContainers = document.querySelectorAll('.image-container');
 
 imageContainers.forEach(imageContainer => {
     const image = imageContainer.querySelector('img');
-    
+    const clickmeImage = imageContainer.querySelector('.clickme-image');
+
     image.addEventListener('click', () => {
-        image.classList.toggle('expanded');
+        if (window.innerWidth > 800) {
+            image.classList.toggle('expanded');
+        }
+    });
+
+    clickmeImage.addEventListener('click', (e) => {
+        e.stopPropagation();
     });
 });
+
 const triggerImage = document.querySelector(".trigger-image");
 const hoverImageContainer = document.querySelector(".hover-image-container");
 const hoverImage = document.querySelector(".hover-image");
 
 triggerImage.addEventListener("mouseover", (e) => {
-    
     const mouseX = e.clientX;
     const mouseY = e.clientY;
 
-   
     hoverImageContainer.style.left = `${mouseX}px`;
     hoverImageContainer.style.top = `${mouseY + 20}px`;
     hoverImageContainer.style.display = "block";
